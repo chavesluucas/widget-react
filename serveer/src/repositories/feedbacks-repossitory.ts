84f21b8d6b518f914d82ed849f2b9cdbc437a22d
Lucas <luucas.chaves@gmail.com>
2022-05-09 15:08:57 -0300

    First Commit

---
+++ serveer/src/repositories/feedbacks-repossitory.ts
@@ -0,0 +1,11 @@
+//isso é o contrato
+
+export interface FeedbackCreateData{
+    type: string,
+    comment: string,
+    screenshot?: string
+}
+
+export interface FeedbacksRepository{
+    create: (data: FeedbackCreateData) => Promise<void>;
+}
