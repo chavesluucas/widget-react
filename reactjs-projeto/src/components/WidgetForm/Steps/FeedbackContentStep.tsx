c35e9c0ba79107a8c5c2893d9e3550ecb6fbbf11
Lucas <luucas.chaves@gmail.com>
2022-05-13 13:14:44 -0300

    Refinamento do widget

--- reactjs-projeto/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ reactjs-projeto/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedBackContentStepProps {
@@ -14,13 +16,20 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
                                         //será string caso exista ou nulo
     const [screenshot, setScreenshot] = useState<string | null>(null);
     const [comment, setComment] = useState('');
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
-    function handleSubmitFeedback(event: FormEvent){
+    async function handleSubmitFeedback(event: FormEvent){
         event.preventDefault();
-        console.log({
+
+        setIsSendingFeedback(true);
+
+        await api.post('/feedbacks',{
+            type: feedbackType,
+            comment,
             screenshot,
-            comment
-        })
+        });
+
+        setIsSendingFeedback(false);
         onFeedbackSent();
     }
 
@@ -68,9 +77,9 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
                     <button
                         type="submit"
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500"
-                        disabled={comment.length === 0}
+                        disabled={comment.length === 0 || isSendingFeedback}
                     >
-                        Enviar Feedback
+                        {isSendingFeedback ? <Loading /> : 'Enviar Feedback'}
                     </button>
                 </footer>
 
